15abd1a3dfcf7f7e657d21313a40b493a6b55eb2
David Rio Deiros <driodeiros@gmail.com>
2020-08-17 10:40:57 -0400

    refactors with hooks

--- client/components/test-mb-next.js
+++ client/components/test-mb-next.js
@@ -1,10 +1,11 @@
 import { Component } from "react";
+import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
 
 const home = [32.782003, -79.932903];
 
-class Map extends Component {
-  state = {
+function Map() {
+  const [state, setState] = useState({
     viewport: {
       width: "100vw",
       height: "100vh",
@@ -12,18 +13,16 @@ class Map extends Component {
       longitude: home[1],
       zoom: 13
     }
-  };
+  });
 
-  render() {
-    return (
-      <ReactMapGL
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxApiAccessToken="pk.eyJ1IjoiZHJpbyIsImEiOiJjanhrczh2c2MyNnVmNDBwNm1ic2NhZTVsIn0.X9AIabxzpa8DYz3D7W0wiQ"
-        onViewportChange={viewport => this.setState({ viewport })}
-        {...this.state.viewport}
-      />
-    );
-  }
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxApiAccessToken="pk.eyJ1IjoiZHJpbyIsImEiOiJjanhrczh2c2MyNnVmNDBwNm1ic2NhZTVsIn0.X9AIabxzpa8DYz3D7W0wiQ"
+      onViewportChange={viewport => setState({ viewport })}
+      {...state.viewport}
+    />
+  );
 }
 
 export default Map;
