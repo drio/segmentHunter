e5587760900a1cec29fcab3bc8c4fc7acf8684e7
David Rio Deiros <driodeiros@gmail.com>
2020-08-25 08:23:19 -0400

    implements first part of the protocol

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,35 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
+import nextCookie from "next-cookies";
+import Router from "next/Router";
 import segments from "../data/segments/all.json";
 import weather from "../data/weather/hourly.json";
 import WeatherSlider from "../components/weather_slider";
+import Layout from "../components/layout";
+
+// FIXME
+const CLIENT_ID = 52300;
+const REDIRECT_PATH = `/exchange_token`;
+const OAUTH_URL = `http://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost/${REDIRECT_PATH}&approval_prompt=force&scope=read`;
+
+const sc_segments_only = segments.filter(s => s.state == "SC");
 
 const importMap = () => import("../components/map");
-
 const Map = dynamic(importMap, {
   ssr: false
 });
 
-const sc_segments_only = segments.filter(s => s.state == "SC");
-
-const App = () => {
+const App = ({ token }) => {
   const [windDirection, setWindDirection] = useState("N");
 
+  useEffect(() => {
+    if (!token) Router.push(OAUTH_URL);
+  }, []);
+
   return (
-    <div>
-      <WeatherSlider
-        data={weather}
-        changeAction={e => {
-          if (e) setWindDirection(e.windDirection);
-        }}
-      />
-      <Map
-        segments={sc_segments_only}
-        weather={weather}
-        windDirection={windDirection}
-      />
-    </div>
+    <Layout>
+      This is the app.
+      {/*
+      <div>
+        <WeatherSlider
+          data={weather}
+          changeAction={e => {
+            if (e) setWindDirection(e.windDirection);
+          }}
+        />
+        <Map
+          segments={sc_segments_only}
+          weather={weather}
+          windDirection={windDirection}
+        />
+      </div>
+      */}
+    </Layout>
   );
 };
+
+export async function getStaticProps(ctx) {
+  const cookies = nextCookie(ctx);
+  return {
+    props: {
+      token: cookies.token ? cookies.token : false
+    }
+  };
+}
 
 export default App;
--- scripts/oauth/step1.sh
+++ scripts/oauth/step1.sh
@@ -2,3 +2,20 @@
 
 source ../../secret/oauth.sh
 open "http://www.strava.com/oauth/authorize?client_id=$CLIENT_ID&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read"
+
+echo 
+(
+	"http://www.strava.com/oauth/authorize?
+client_id=$CLIENT_ID&
+response_type=code&
+redirect_uri=http://localhost/exchange_token&
+approval_prompt=force
+&scope=read"
+
+  "https://www.strava.com/oauth/authorize?
+response_type=code&
+redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fauth0&
+scope=openid%20email%20profile&
+state=ee5a94ab1cc6cbbcb5c46e278ddaf31260abd5fbe51b73ec4846dd9bfe3aa01a&
+client_id=52300"
+) > /dev/null
