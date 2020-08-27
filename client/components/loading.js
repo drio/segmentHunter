311687167c03b2f5bbb672a398ea9a2bf704e2c6
David Rio Deiros <driodeiros@gmail.com>
2020-08-27 12:07:54 -0400

    adds a fix UI changes

--- client/components/loading.js
+++ client/components/loading.js
@@ -3,7 +3,9 @@ import Layout from "../components/layout";
 const Loading = () => {
   return (
     <Layout>
+      <div className="content"></div>
       <div className="center">
+        <div className="title is-2 is-family-primary">Strava Hunter</div>
         <div>
           <figure className="image">
             <img src="loading/loading.gif" alt="Image" />
@@ -11,7 +13,7 @@ const Loading = () => {
         </div>
         <br />
         <div className="content">
-          <div className="title is-4">Loading data ...</div>
+          <div className="is-5">Loading data ...</div>
         </div>
       </div>
     </Layout>
