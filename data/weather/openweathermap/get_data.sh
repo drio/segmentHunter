e91eafe62d0e325337b0d5d06e09a214314040f9
David Rio Deiros <driodeiros@gmail.com>
2020-08-31 14:00:22 -0400

    nc

--- data/weather/openweathermap/get_data.sh
+++ data/weather/openweathermap/get_data.sh
@@ -5,6 +5,7 @@ source ../../../secret/owm.sh
 lat=32.784618
 lon=-79.940918
 
+#curl -v 'https://api.openweathermap.org/data/2.5/onecall?lat=32.784618&lon=-79.940918&APPID=92f710577a529b24207f0d0c0b3e0971' > data.json
 cat <<EOF
-curl -v https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&APPID=$OPM_KEY > ./data.json
+curl -v  'https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&APPID=$OPM_KEY&exclude=minutely,daily,current&units=metric' > ./data.json
 EOF
